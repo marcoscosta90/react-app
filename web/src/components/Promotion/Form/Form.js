7735584e34716219d7b25024c01ba9032d2706ed
marcoscosta90 <marcoscosta.dev@gmail.com>
2020-12-16 16:57:53 -0300

    Formjs updated

--- web/src/components/Promotion/Form/Form.js
+++ web/src/components/Promotion/Form/Form.js
@@ -54,34 +54,41 @@ const PromotionForm = ( { id }) => {
    
     
 
-    return (
-    <div> 
-        <h1>Promo Show</h1>
-        <h2>Nova Promoção</h2>
+   
 
-        <form onSubmit={onSubmit}>
+  return (
+    <div>
+      <h1>Promo Show</h1>
+      <h2>Nova Promoção</h2>
+      {!values
+        ? (
+          <div>Carregando...</div>
+        ) : (
+          <form onSubmit={onSubmit}>
+            {saveInfo.loading && <span>Salvando dados...</span>}
             <div className="promotion-form__group">
-                <label htmlFor="title">Título</label>
-                <input id="title" name="title" type="text" onChange={onChange} value={values.title}/>
+              <label htmlFor="title">Título</label>
+              <input id="title" name="title" type="text" onChange={onChange} value={values.title} />
             </div>
             <div className="promotion-form__group">
-                <label htmlFor="url">Link</label>
-                <input id="url" name="url" type="text" onChange={onChange} value={values.url}/>
+              <label htmlFor="url">Link</label>
+              <input id="url" name="url" type="text" onChange={onChange} value={values.url} />
             </div>
             <div className="promotion-form__group">
-                <label htmlFor="imageUrl">Imagem Url </label>
-                <input id="imageUrl" name="imageUrl" type="text" onChange={onChange} value={values.imageUrl}/>
+              <label htmlFor="imageUrl">Imagem (URL)</label>
+              <input id="imageUrl" name="imageUrl" type="text" onChange={onChange} value={values.imageUrl} />
             </div>
             <div className="promotion-form__group">
-                <label htmlFor="price">Preço </label>
-                <input id="price" name="price" type="number" onChange={onChange} value={values.price}/>
+              <label htmlFor="price">Preço</label>
+              <input id="price" name="price" type="number" onChange={onChange} value={values.price} />
             </div>
             <div>
-                <button type="submit">Salvar</button>
+              <button type="submit">Salvar</button>
             </div>
-        </form>
+          </form>
+        )}
     </div>
-    )
+  )
 };
 
 export default PromotionForm;
