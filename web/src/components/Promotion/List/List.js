a842efb6a5e501e6d6e0177e4df2cdd8ec45f35c
marcoscosta90 <marcoscosta.dev@gmail.com>
2020-12-17 14:18:28 -0300

    Implemented saving and listing comments in a modal

--- web/src/components/Promotion/List/List.js
+++ web/src/components/Promotion/List/List.js
@@ -1,36 +1,38 @@
 import React, { useState } from 'react';
 import PromotionCard from 'components/Promotion/Card/Card';
-import UIModal from 'components/UI/Modal/Modal'
+import PromotionModal from 'components/Promotion/Modal/Modal';
 import './List.css'
 
 const PromotionList = ({ loading, promotions, error }) => {
     const [promotionId, setPromotionId] = useState(null);
 
 
-if(error){
-    return <div> Algo de errado nao esta certo</div>
-}
+    if (error) {
+        return <div> Algo de errado nao esta certo</div>
+    }
 
-    if(loading || promotions === null) {
+    if (loading || promotions === null) {
         return <div>Carregando....</div>
     }
 
-    if(promotions.length === 0) {
+    if (promotions.length === 0) {
         return <div> Nenhum resultado encontrado</div>
     }
 
 
     return (
-        <div className="promotion-list"> 
-             {promotions.map((promotion) => (
-                 <PromotionCard 
-                 promotion={promotion} 
-                  onClickComments={() => setPromotionId(promotion.id)}
-                  />
-             ))}
-             <UIModal isOpen={Boolean(promotionId)} onClickClose={() => setPromotionId(null)}>
-                  <h1>Comentários</h1>
-             </UIModal>
+        <div className="promotion-list">
+            {promotions.map((promotion) => (
+                <PromotionCard
+                    promotion={promotion}
+                    onClickComments={() => setPromotionId(promotion.id)}
+                />
+            ))}
+            {promotionId && (            
+            <PromotionModal
+                promotionId={promotionId}
+                onClickClose={() => setPromotionId(null)} />
+                )}
         </div>
     )
 }
---
+++ web/src/components/Promotion/Modal/Modal.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import UIModal from 'components/UI/Modal/Modal';
+import useApi from 'components/utils/useApi';
+import PromotionModalCommentsTree from 'components/Promotion/Modal/CommentsTree/CommentsTree';
+import './Modal.css';
+
+const PromotionModal = ({ promotionId, onClickClose }) => {
+    const [comment, setComment] = useState('');
+    const [load, loadInfo] = useApi({
+        url: '/comments',
+        params: {
+            promotionId,
+            _expand: 'user'
+        }
+    });
+
+    const [sendComment, sendCommentInfo] = useApi({
+        url: '/comments',
+        method: 'POST',
+    });
+
+    useEffect(() => {
+        load();
+        // eslint-disable-next-line    
+    }, []);
+
+    async function onSubmit(ev) {
+        ev.preventDefault();
+        try {
+            await sendComment({
+                data: {
+                    userId: 1,
+                    promotionId,
+                    comment
+                }
+            });
+            setComment('');
+            load();
+        } catch (e) {
+
+        }
+
+
+    }
+
+
+
+    return (
+        <UIModal isOpen onClickClose={onClickClose}>
+            <form className="promotion-modal__comment-form" onSubmit={onSubmit}>
+                <textarea
+                    placeholder="Comentar..."
+                    onChange={(ev) => setComment(ev.target.value)}
+                    value={comment} />
+                <button
+                    type="submit"
+                    disabled={sendCommentInfo.loading}>
+                    {sendCommentInfo.loading ? 'Enviando' : 'Enviar'}</button>
+            </form>
+            <PromotionModalCommentsTree comments={loadInfo.data} />
+        </UIModal>
+    )
+
+
+}
+
+
+export default PromotionModal;
